929bd2ff560d3d67f0c9a0fecf4ade9236287aab
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-06-12 00:58:37 +0900

    コードのリファクタリングをした。可読性を上げた。

--- SampleDxLib_04/ソース.cpp
+++ SampleDxLib_04/ソース.cpp
@@ -1,6 +1,5 @@
 #include "DxLib.h"
 #include <cmath>
-#include <ctime>
 
 const float DEGTORAD = 0.017453293f;
 
@@ -18,66 +17,22 @@ int WINAPI WinMain(_In_ HINSTANCE , _In_opt_ HINSTANCE , _In_ LPSTR , _In_ int)
 
 	int color      = GetColor(255, 0, 0);
 
-	bool colorChange = false;
-
-	
-	//srand関数を使ってランダムな値を初期化する。
-	srand(static_cast<unsigned int>(time(nullptr)));
-
-
 	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0)
 	{
 
-		if (!colorChange)
-		{
-			int object = DrawCircle(PlayerX, PlayerY, 10, color);
-		}
-		else
-		{
-			int object = DrawCircle(PlayerX, PlayerY, 10, GetColor(255 , 0 , 0));
-		}
-
-		
+		int object = DrawCircle(PlayerX, PlayerY, 10, color);
 
 		PlayerX += AddVectorX;
 		PlayerY += AddVectorY;
 
 		if (640 < PlayerX || PlayerX < 0)
 		{
-			//画面の端（640地点）に到達したら、Xベクトルを反転させる。
 			AddVectorX *= -1;
-
-			if (!colorChange)
-			{
-				int red   = rand() % 256;
-				int green = rand() % 256;
-				int blue  = rand() % 256;
-
-				int color = GetColor(red, green, blue);
-
-				colorChange = true;
-			}
 		}
 
 		if (480 < PlayerY || PlayerY < 0)
 		{
 			AddVectorY *= -1;
-
-			if (!colorChange)
-			{
-				int red   = rand() % 256;
-				int green = rand() % 256;
-				int blue  = rand() % 256;
-
-				int color = GetColor(red, green, blue);
-
-				colorChange = true;
-			}
-		}
-
-		if (colorChange)
-		{
-			colorChange = false;
 		}
 
 	}
